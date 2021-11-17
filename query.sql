92873c5d3e51e24535bf5b56b478a72f4c702985
Alejandro Cesar Moya <46249970+e1ectriccrasher@users.noreply.github.com>
2021-11-17 00:15:54 -0600

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,7 @@
--- Una aplicación frecuente de Ciencia de Datos aplicada a la industria del microlending es el de calificaciones crediticias (credit scoring). Puede interpretarse de muchas formas: propensión a pago, probabilidad de default, etc. La intuición nos dice que las variables más importantes son el saldo o monto del crédito, y la puntualidad del pago; sin embargo, otra variable que frecuentemente escapa a los analistas es el tiempo entre cada pago. La puntualidad es una pésima variable para anticipar default o inferir capacidad de pago de micropréstamos, por su misma naturaleza. Si deseamos examinar la viabilidad de un producto de crédito para nuestras videorental stores:
+-- Una aplicación frecuente de Ciencia de Datos aplicada a la industria del microlending es el de calificaciones crediticias (credit scoring). 
+--Puede interpretarse de muchas formas: propensión a pago, probabilidad de default, etc. 
+--La intuición nos dice que las variables más importantes son el saldo o monto del crédito, y la puntualidad del pago; sin embargo, otra variable que frecuentemente escapa a los analistas es el tiempo entre cada pago. 
+--La puntualidad es una pésima variable para anticipar default o inferir capacidad de pago de micropréstamos, por su misma naturaleza. Si deseamos examinar la viabilidad de un producto de crédito para nuestras videorental stores:
 
 --Cuál es el promedio, en formato human-readable, de tiempo entre cada pago por cliente de la BD Sakila?
 --Sigue una distribución normal?
